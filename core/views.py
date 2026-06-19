b833367088bf7f953f86d296d031e1d5e68ca83f
João Pedro <joao.guadalupe06@gmail.com>
2026-06-19 18:33:56 -0300

    feat: rotas api e adição do modulo de segurança do django

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Patient, Consultation
+from .serializers import PatientSerializer, ConsultationSerializer
+
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+class ConsultationViewSet(viewsets.ModelViewSet):
+    queryset = Consultation.objects.all()
+    serializer_class = ConsultationSerializer
